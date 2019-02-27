709a7b1c590339ddf5440abf171267e9fe0482d3
noobtogod <lxiaojian1@gmail.com>
2019-02-27 15:57:05 +0800

    add service

---
+++ src/main/java/com/wemarket/wac/biz/service/LoginService.java
@@ -0,0 +1,7 @@
+package com.wemarket.wac.biz.service;
+
+/**
+ * Created by justinli on 2019/2/27
+ **/
+public interface LoginService {
+}
---
+++ src/main/java/com/wemarket/wac/biz/task/WacTask.java
@@ -0,0 +1,7 @@
+package com.wemarket.wac.biz.task;
+
+/**
+ * Created by justinli on 2019/2/12
+ **/
+public class WacTask {
+}
